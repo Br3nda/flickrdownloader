4a462a23cbfdbf2453a0e2f60db778b0176d1396
Brenda Wallace <shiny@cpan.org>
2013-05-14 22:51:01 +1200

    fetches private video and photos

--- getphotos.py
+++ getphotos.py
@@ -1,72 +1,99 @@
 import flickrapi, os, urllib, argparse
 from datetime import date
 from _config import apikey, apisecret
+import shutil
 
 ordinal = 734100
-days_per_fetch=30
-
-def retrieve(photo, filename):
-	info = flickr.photos_getInfo(photo_id=photo.get('id'), secret=photo.get('secret'))
-	originalsecret = info[0].get('originalsecret')
-
-	#http://farm9.staticflickr.com/8518/8366707744_94cd8f8f01_o.jpg
-	#http://farm9.staticflickr.com/8518/8366707744_94cd8f8f01_o.jpg
-        if photo.get('originalsecret'):
-
-		url = 'http://farm{farm}.staticflickr.com/{server}/{id}_{secret}_o.jpg'.format(
-			farm=photo.get('farm'),
-			server=photo.get('server'),
-			id=photo.get('id'),
-			secret=originalsecret)
-	else:
-		url = 'http://farm{farm}.staticflickr.com/{server}/{id}_{secret}_o.jpg'.format(
-			farm=photo.get('farm'),
-			server=photo.get('server'),
-			id=photo.get('id'),
-			secret=secret)
-	print "\t" + url
-
-	try:
-		urllib.urlretrieve(url, filename)
-	except Exception, e:
-		print e
+days_per_fetch=365
 
 
+print apikey
+print apisecret
 
+import logging
+flickrapi.set_log_level(logging.DEBUG)
 flickr = flickrapi.FlickrAPI(apikey, secret=apisecret)
+
+(token, frob) = flickr.get_token_part_one(perms='read')
+if not token: raw_input("Press ENTER after you authorized this program")
+flickr.get_token_part_two((token, frob))
+
+print "token = %s"%(token)
+
 while True:
 
-	#work out the dates
-	from_date = date.fromordinal(ordinal)
-	to_date = date.fromordinal(ordinal + days_per_fetch)
-	print "fetching {fromdate} to {todate}".format(
-		fromdate=from_date.strftime("%Y-%m-%d"),
-		todate=to_date.strftime("%Y-%m-%d"),
-		privacy_filter=3)
-	ordinal -= days_per_fetch
+    #work out the dates
+##    from_date = date.fromordinal(ordinal)
+##    to_date = date.fromordinal(ordinal + days_per_fetch)
+##    print "fetching {fromdate} to {todate}".format(
+##        fromdate=from_date.strftime("%Y-%m-%d"),
+##        todate=to_date.strftime("%Y-%m-%d"),
+##        privacy_filter=3)
+        
+    ordinal += days_per_fetch
 
-	#fetch the photos
-	for photo in flickr.walk(user_id='36251685@N00',
-	        min_taken_date=from_date.strftime("%Y-%m-%d"),
-	        max_taken_date=to_date.strftime("%Y-%m-%d")):
-	    
-		#print "Getting info"
-		photo_id = photo.get('id')
-		secret = photo.get('secret')
-		orig_secret = photo.get('originalsecret')
+    #fetch the photos
+##    for photo in flickr.walk(user_id='36251685@N00',
+##        min_taken_date=from_date.strftime("%Y-%m-%d"),
+##        max_taken_date=to_date.strftime("%Y-%m-%d"),
+##        media='videos',
+##        privacy_filter=3):
+    for photo in flickr.photos_search(
+            user_id='36251685@N00',
+##        min_taken_date=from_date.strftime("%Y-%m-%d"),
+##        max_taken_date=to_date.strftime("%Y-%m-%d"),
+            media='videos',
+            privacy_filter='3',
+            per_page=10)[0]:
+            
+##    min_taken_date=2010-11-24&max_taken_date=2011-11-24&privacy_filter=3&media=videos&format=rest
+#&auth_token=72157633468807171-867d9080b670bd57&api_sig=3119ccff4d38e61f55b89880c2693bc0
+        
+        if photo.get('ispublic'):
+            print "Public!"
+        else:
+            print "Private"
+        
+    
 
-		folder = '{year}-{month}'.format(year=from_date.strftime("%Y"), month=from_date.strftime("%m"))
-		if not os.path.exists(folder):
-			os.mkdir(folder)
-
-		filename = "{folder}/{photo_id}.jpg".format(folder=folder, photo_id=photo_id)
-		old_filename = '{folder}/{photo_id}_{secret}_o.jpg'.format(folder=folder, secret=orig_secret, photo_id=photo_id)
-		
-		print "\t" + filename,
-		if os.path.exists(filename) or os.path.exists(old_filename):
-			print "\tskipping"
-
-		else:
-                	retrieve(photo, filename=filename)
-
-	
+##        #print "Getting info"
+##        photo_id = photo.get('id')
+##        secret = photo.get('secret')
+##        orig_secret = photo.get('originalsecret')
+##
+##        folder = '{year}-{month}'.format(year=from_date.strftime("%Y"), month=from_date.strftime("%m"))
+##        if not os.path.exists(folder):
+##            os.mkdir(folder)
+##
+##        filename = "{folder}/{photo_id}.jpg".format(folder=folder, photo_id=photo_id)
+##        old_filename = '{folder}/{photo_id}_{secret}_o.jpg'.format(folder=folder, secret=orig_secret, photo_id=photo_id)
+##
+##        if os.path.exists(filename) or os.path.exists(old_filename):
+##            print "\tskipping"
+##
+##        else:
+##            sizes = flickr.photos_getSizes(photo_id=photo.get('id'), secret=photo.get('secret'))[0]
+##            url = sizes[-1].get('source')
+##            media = sizes[-1].get('media')
+##            
+##            
+##            print "\t" + url
+##            if media == 'video':
+##                (tmp_filename, headers) = urllib.urlretrieve(url)
+##                
+##                #touch the jpeg filename, so this video isn't re-fetched
+##                f = open(filename, 'w')
+##                f.write('.')                
+##                
+##                #read the original filenamefrom the http headers
+##                cd =headers['Content-Disposition']
+##                filename = cd.replace('attachment; filename=', '{folder}/'.format(folder=folder))
+##                
+##                #move to real filename
+##                shutil.move(tmp_filename, filename)
+##                
+##
+##            else:
+##                (filename, headers) = urllib.urlretrieve(url, filename)
+##            print headers
+    assert False
